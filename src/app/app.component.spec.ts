155ef9f220c7abb9eff62bdc312fa1f2500aef7c
JorikWeDoIT <jorik@wedoit.be>
2022-10-17 12:47:24 +0200

    added test for filtering and adding

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -2,9 +2,11 @@ import {getTestBed, TestBed} from '@angular/core/testing';
 import { AppComponent } from './app.component';
 import {HttpClient, HttpClientModule} from "@angular/common/http";
 import {HttpClientTestingModule} from "@angular/common/http/testing";
+import {Pokemon} from "./models/pokemon.model";
+import {Sprites} from "./models/sprites.model";
+import {Stats} from "./models/stats.model";
 
 describe('AppComponent', () => {
-
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [
@@ -26,5 +28,20 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('Task2-PokemonTeam-Jorik');
+  });
+
+  it('should return an array with only the filtered pokemon', function () {
+    const pokemons = [
+      new Pokemon('pokemon1', 0, new Sprites('', '', '', ''), '', '', new Stats(0, 0, 0, 0, 0, 0)),
+      new Pokemon('pokemon2', 0, new Sprites('', '', '', ''), '', '', new Stats(0, 0, 0, 0, 0, 0)),
+      new Pokemon('pokemon3', 0, new Sprites('', '', '', ''), '', '', new Stats(0, 0, 0, 0, 0, 0))
+    ]
+
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    app.pokemonList = pokemons;
+    app.filterPokemonByName('1')
+
+    expect(app.filteredPokemonList.length === 1);
   });
 });
--- src/app/components/pokemon-details/pokemon-details.component.spec.ts
+++ src/app/components/pokemon-details/pokemon-details.component.spec.ts
@@ -1,11 +1,17 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { PokemonDetailsComponent } from './pokemon-details.component';
-import {provideMockStore} from "@ngrx/store/testing";
+import {MockStore, provideMockStore} from "@ngrx/store/testing";
+import {addPokemonToTeam} from "../../store/pokemon.actions";
+import {Pokemon} from "../../models/pokemon.model";
+import {Sprites} from "../../models/sprites.model";
+import {Stats} from "../../models/stats.model";
 
 describe('PokemonDetailsComponent', () => {
   let component: PokemonDetailsComponent;
   let fixture: ComponentFixture<PokemonDetailsComponent>;
+
+  let store: MockStore;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -17,9 +23,30 @@ describe('PokemonDetailsComponent', () => {
     fixture = TestBed.createComponent(PokemonDetailsComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    store = TestBed.inject(MockStore);
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should dispatch action when selectedPokemon is not null', function () {
+    //get selectedPokemon from store
+
+    const dispatchSpy = spyOn(store, 'dispatch').and.callThrough();
+    const AddToTeamButtonSpy = spyOn(component, 'addPokemonToTeam').and.callThrough();
+    expect(AddToTeamButtonSpy).not.toHaveBeenCalled();
+
+    let button = fixture.nativeElement.querySelector('#addPokemon');
+    expect(button).toBeTruthy();
+    expect(button.textContent.trim()).toBe('Add to team');
+
+    const app = fixture.componentInstance;
+    app.selectedPokemon$ = new Pokemon('pokemon1', 0, new Sprites('', '', '', ''), '', '', new Stats(0, 0, 0, 0, 0, 0));
+    app.myTeam$ = [];
+    button.click();
+    expect(AddToTeamButtonSpy).toHaveBeenCalled();
+    //expect dispatched action for addPokemonToTeam
+    expect(dispatchSpy).toHaveBeenCalledWith(addPokemonToTeam({pokemon:app.selectedPokemon$}));
   });
 });
